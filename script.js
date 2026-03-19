6df30adbe743bb79d2e7d669c675a8b49b793fed
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-03-19 18:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Complete the js code
 function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
-
-Car.prototype.getMakeModel1 = function() {
+Car.prototype.getMakeModel = function() {
 	return this.make + " " + this.model;
-}
-
+};
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
 	this.topSpeed = topSpeed;
@@ -17,8 +14,6 @@ SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function() {
 	return this.topSpeed;
-}
-
-// Do not change the code below
+};// Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
